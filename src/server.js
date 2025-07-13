7fac1820e034bdf7c8ab341011ec5ed6c95448df
Bohdan <guestgeron@gmail.com>
2025-07-13 20:56:40 +0300

    add 7 homework

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
+import multer from 'multer';
+import fs from 'node:fs';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import pino from 'pino';
 import pinoHttp from 'pino-http';
+import swaggerUi from 'swagger-ui-express';
 
-import multer from 'multer';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { authRouter } from './routers/auth.js';
@@ -17,6 +20,13 @@ const upload = multer({ dest: 'uploads/' });
 export function setupServer() {
   const app = express();
   const logger = pino();
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../docs/swagger.json'), 'utf8')
+  );
+
   app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
   app.use(cors());
   app.use(express.json());
@@ -25,6 +35,7 @@ export function setupServer() {
 
   app.use('/contacts', upload.single('photo'), contactsRouter);
   app.use('/auth', authRouter);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use(notFoundHandler);
   app.use(errorHandler);
